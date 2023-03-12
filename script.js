644ea2bd9b0d01f54fca2b3ec37a856a3fcee28e
Vlad <jeywer2747@gmail.com>
2023-03-12 03:35:00 +0300

    fix bugs

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 
-window.onload = function()
-{
+window.onload = function() {
 let maxBottom = 900;
 let day = (new Date()).getDate();
 let currentBottom = 900 -  day / 30 * maxBottom;
 document.getElementById("value").style.bottom = currentBottom + 'px';
+
+
 };
